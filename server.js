f7d5359c00de2063693eaa6b07b108f3f80d3761
BIraj <birag.gpta@gmail.com>
2021-03-02 23:57:41 +0530

    path corrected

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const port = process.env.PORT || 3000;
 const app = express();
-const publicPath = path.join(__dirname, "..", "public");
+const publicPath = path.join(__dirname, "client", "public");
 app.use(express.static(publicPath));
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
